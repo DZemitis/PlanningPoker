aa477c8b5501c78523de24624373928ab4e5f365
DavisZemitis <davis.zemitis@28stone.com>
2022-04-21 12:54:37 +0300

    Added DataAcces.Models
    Created GameRoomDto

---
+++ ScrumPoker.DataAcces.Models/Models/GameRoomDto.cs
@@ -0,0 +1,7 @@
+namespace ScrumPoker.DataAcces.Models.Models;
+
+public class GameRoomDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- ScrumPoker.DataAcces/Data/DataBase.cs
+++
@@ -1,9 +0,0 @@
-using ScrumPoker.Core.Models;
-
-namespace ScrumPoker.Data.Data;
-
-public static class DataBase
-{
-    public static List<Player> Players = new List<Player>();
-    public static List<VotingResults> VotingResults = new List<VotingResults>();
-}
