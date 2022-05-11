901747f09ab7b64e22712147ec45ecfc193b441f
DavisZemitis <davis.zemitis@28stone.com>
2022-05-11 15:12:31 +0300

    cleaned up exception contracts

--- ScrumPoker.Common/ConflictException.cs
+++ ScrumPoker.Common/ConflictException.cs
@@ -2,15 +2,10 @@ namespace ScrumPoker.Common;
 
 public class ConflictException : ScrumPokerException
 {
-    public ConflictException(int statusCode, string message) =>
-        (StatusCode, Message) = (statusCode, message);
+    public ConflictException(string message) =>
+        (Message) = (message);
     
     protected ConflictException()
-    {
-        
-    }
-
-    protected ConflictException(string message) : base(message)
     {
         
     }
