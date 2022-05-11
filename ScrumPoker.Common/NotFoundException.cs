e29fc10a7fd0231c586dd3bceef0a09dfb40bf87
DavisZemitis <davis.zemitis@28stone.com>
2022-05-11 15:12:31 +0300

    cleaned up exception contracts

--- ScrumPoker.Common/NotFoundException.cs
+++ ScrumPoker.Common/NotFoundException.cs
@@ -2,15 +2,10 @@ namespace ScrumPoker.Common;
 
 public class NotFoundException : ScrumPokerException
 {
-    public NotFoundException(int statusCode, string message) =>
-        (StatusCode, Message) = (statusCode, message);
+    public NotFoundException(string message) =>
+        (Message) = (message);
     
     protected NotFoundException()
-    {
-        
-    }
-
-    protected NotFoundException(string message) : base(message)
     {
         
     }
--- ScrumPoker.Common/ScrumPokerException.cs
+++ ScrumPoker.Common/ScrumPokerException.cs
@@ -4,21 +4,11 @@ namespace ScrumPoker.Common;
 
 public class ScrumPokerException : Exception
 {
-    public ScrumPokerException(int statusCode, string message) =>
-        (StatusCode, Message) = (statusCode, message);
-
-    public int StatusCode { get; set; }
-
-    public object? Value { get; set; }
-    
+    public ScrumPokerException(string message) =>
+        (Message) = (message);
     public new string? Message { get; set; }
 
     protected ScrumPokerException()
-    {
-        
-    }
-
-    protected ScrumPokerException(string message) : base(message)
     {
         
     }
