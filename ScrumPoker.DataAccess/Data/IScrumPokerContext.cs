e7e2c4fc45e8a69128e10075847953296a9f0899
DavisZemitis <davis.zemitis@28stone.com>
2022-05-18 15:53:32 +0300

    cleanup

--- ScrumPoker.DataAccess/Data/IScrumPokerContext.cs
+++ ScrumPoker.DataAccess/Data/IScrumPokerContext.cs
@@ -6,8 +6,6 @@ namespace ScrumPoker.DataAccess.Data;
 
 public interface IScrumPokerContext
 {
-    DbSet<T> Set<T>() where T : class;
-    EntityEntry<T> Entry<T>(T entity) where T : class;
     DbSet<PlayerDto> Players { get; set; }
     DbSet<GameRoomDto> GameRooms { get; set; }
     int SaveChanges();
