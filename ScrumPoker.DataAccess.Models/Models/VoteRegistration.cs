147a7ae64d455f526334f03f204fa6a6203549bc
DavisZemitis <davis.zemitis@28stone.com>
2022-05-25 10:39:27 +0300

    added vote review

--- ScrumPoker.DataAccess.Models/Models/VoteRegistration.cs
+++ ScrumPoker.DataAccess.Models/Models/VoteRegistration.cs
@@ -2,6 +2,7 @@ namespace ScrumPoker.DataAccess.Models.Models;
 
 public class VoteRegistration
 {
+    public int Id { get; set; }
     public int PlayerId { get; set; }
     public int GameRoomId { get; set; }
     public int Vote { get; set; }
